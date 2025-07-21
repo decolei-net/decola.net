ee4baf4ea7e4c5fb12740ae609227d2498e2be94
Eduardo da Silva Bezerra <e.da.silva.bezerra@avanade.com>
2025-07-20 21:07:09 -0300

    feat: Adiciona CRUD com endpoint completo de Reservas

---
+++ DTOs/ViajanteDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Decolei.net.DTOs
+{
+    public class ViajanteDto
+    {
+        [Required(ErrorMessage = "O nome do viajante é obrigatório.")]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O documento do viajante é obrigatório.")]
+        [StringLength(50)]
+        public string Documento { get; set; }
+    }
+}
