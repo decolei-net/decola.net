4e480973779c9d358ec211030f4c8f0ed27d2c13
Leonardo Amyntas Filho <l.de.freitas.filho@AVAPC-029750502>
2025-07-22 13:46:40 -0300

    migrations

--- Migrations/20250721194857_NovaMigration.Designer.cs
+++ Migrations/20250721194857_NovaMigration.Designer.cs
@@ -4,6 +4,7 @@ using Decolei.net.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Decolei.net.Migrations
 {
     [DbContext(typeof(DecoleiDbContext))]
-    partial class DecoleiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250721194857_NovaMigration")]
+    partial class NovaMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250721194857_NovaMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Decolei.net.Migrations
+{
+    /// <inheritdoc />
+    public partial class NovaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Reserva_StatusPagamento",
+                table: "Reserva",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reserva_StatusPagamento",
+                table: "Reserva");
+        }
+    }
+}
